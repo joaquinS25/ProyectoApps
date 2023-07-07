a0dab9020fadabca5ce7315cc33252f0e5de346f
Joaquin Salas <mitroncito25@hotmail.com>
2023-07-06 20:20:53 -0500

    MODIFICACIONES

--- app/src/main/java/com/example/proyectoapps/OpcionesAdministrador.java
+++ app/src/main/java/com/example/proyectoapps/OpcionesAdministrador.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class LoginAdministrador extends AppCompatActivity {
-    Button btnIniciarSesion;
+public class OpcionesAdministrador extends AppCompatActivity {
+    Button btnUsers,btnPeliculas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_administrador);
+        setContentView(R.layout.activity_opciones_administrador);
         asignarReferencias();
     }
     private void asignarReferencias(){
-        btnIniciarSesion=findViewById(R.id.btnIniciarSesion);
-        btnIniciarSesion.setOnClickListener(v -> {
-            Intent intent =new Intent(this, OpcionesAdministrador.class);
+        btnUsers=findViewById(R.id.btnUsers);
+        btnUsers.setOnClickListener(v -> {
+            Intent intent =new Intent(this, ListarUsuarios.class);
             startActivity(intent);
         });
     }
